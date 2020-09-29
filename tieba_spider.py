aea050505d926c01a145dc9c60d843b2c1e8cc43
Young <13760744647@163.com>
2020-09-29 10:12:24 +0800

    贴吧爬虫

--- tieba_spider.py
+++ tieba_spider.py
@@ -1,42 +1,33 @@
 """
+  @Author : zhangyong2625
+  @Email : 1293271923@qq.com
+
 https://tieba.baidu.com/p/6972849748?red_tag=2913427813
-1.在浏览器中查看网页源代码
-2.使用python读文本文件
-3.正则表达式的应用
-4.先抓大再抓小的匹配技巧
-5.使用Python写CSV文件
+1.获取网页源代码
+2.正则表达式的应用
+3.先抓大再抓小的匹配技巧
+4.使用Python写CSV文件
 """
 import re
 import csv
+import requests
 
-# 将
-def list_to_dict(user,content,column):
-    count = min(len(user),len(content))
-    lst = []
-    for i in range(count):
-        d = {}
-        d[column[0]] = user[i]
-        d[column[1]] = content[i]
-        lst.append(d)
-    return lst
 
-def result(file,column):
-    file = r"source.txt"
-    with open(file, "r", encoding="utf-8") as f:
-        content = f.read()
+def result(content):
     pattern_user = 'username="(.*?)" class='
-    pattern_content = '(\S*?)</div><br>'
     user = re.findall(pattern_user, content, re.S)
-    content = re.findall(pattern_content, content, re.S)
-    return list_to_dict(user,content,column)
+    print(user)
+    return user
 
-def tieba(file,column):
-    with open("tieba.csv","w",newline='',encoding="utf-8") as f:
-        writer = csv.DictWriter(f,fieldnames=column)
+
+def tieba(file):
+    with open("tieba.csv", "w", newline='', encoding="utf-8") as f:
+        writer = csv.DictWriter(f, fieldnames="name")
         writer.writeheader()
-        writer.writerows(result(file,column))
+        writer.writerows(result(file))
+
 
 if __name__ == "__main__":
-    file = "source.txt"
-    column = ['name','content']
-    tieba(file,column)
+    url = r"https://tieba.baidu.com/p/6972849748?red_tag=2913427813"
+    source = requests.get(url).content.decode("utf-8")
+    tieba(source)
